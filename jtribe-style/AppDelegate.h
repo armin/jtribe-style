9121163eb7d2e00b8b2a45d211e32d345d4807c2
Armin Kroll <armin@jtribe.com.au>
2015-04-09 14:50:30 +1000

    Initial Commit

---
+++ jtribe-style/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  jtribe-style
+//
+//  Created by Armin Kroll on 9/04/2015.
+//  Copyright (c) 2015 jtribe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ jtribe-style/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  jtribe-style
+//
+//  Created by Armin Kroll on 9/04/2015.
+//  Copyright (c) 2015 jtribe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
